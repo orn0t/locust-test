fb12b9d2663e2a77d5e64ba67c93f54f84b4bbd7
Alexander Gor <ornot.work@gmail.com>
2020-10-18 14:25:51 +0300

    updated for new locust

--- locustfile.py
+++ locustfile.py
@@ -2,12 +2,16 @@ import os
 from locust import TaskSet, task, between, HttpUser
 
 
-class CustodianTaskSet(TaskSet):
+class LegalLocust(HttpUser):
     wait_time = between(5, 15)
+
+    headers = {
+        "Authentication": os.environ.get("LOCUST_TOKEN")
+    }
 
     @task(10)
     def get_clients_list_depth_1(self):
-        self.client.get("/custodian/data/client?depth=1", headers=self.locust.headers)
+        self.client.get("/custodian/data/client?depth=1", headers=self.headers)
 
     # @todo: clients request depth > 1 requests causes fails on RPS > 3, may rely on amount of DB sessions
 
@@ -21,32 +25,20 @@ class CustodianTaskSet(TaskSet):
 
     @task(30)
     def get_employee_position(self):
-        self.client.get("/custodian/data/employee_position/1", headers=self.locust.headers)
+        self.client.get("/custodian/data/employee_position/1", headers=self.headers)
 
     @task(10)
     def get_employee_positions(self):
-        self.client.get("/custodian/data/employee_position?depth=1", headers=self.locust.headers)
+        self.client.get("/custodian/data/employee_position?depth=1", headers=self.headers)
 
     @task(10)
     def get_employee_positions_d2(self):
-        self.client.get("/custodian/data/employee_position?depth=2", headers=self.locust.headers)
+        self.client.get("/custodian/data/employee_position?depth=2", headers=self.headers)
 
     @task(20)
     def get_documents_d1(self):
-        self.client.get("/custodian/data/document?depth=1", headers=self.locust.headers)
+        self.client.get("/custodian/data/document?depth=1", headers=self.headers)
 
     @task(20)
     def get_documents_d2(self):
-        self.client.get("/custodian/data/document?depth=2", headers=self.locust.headers)
-
-
-class FastTasksSet(TaskSet):
-    tasks = {CustodianTaskSet, }
-
-
-class LegalLocust(HttpUser):
-    task_set = FastTasksSet
-
-    headers = {
-        "Authentication": os.environ.get("LOCUST_TOKEN")
-    }
+        self.client.get("/custodian/data/document?depth=2", headers=self.headers)
